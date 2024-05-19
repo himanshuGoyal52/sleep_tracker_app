988edebee5d7603ce270d0a8977de94c6127927b
Himanshu <goyalhimanshu424@gmail.com>
2024-05-19 17:20:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config.js';
+import sleepRouter from './routers/sleepRouter.js';
+
+
+// connection to mongoDB
+async function main(){
+    await mongoose.connect(config.MONGODB_URL);
+}
+main().catch(err => console.log(err));
+main().then(() => {
+    console.log("Connected to mongodb database");
+})
+// connection to mongoDB
+
+const app = express();
+app.use(express.json());
+
+app.use('/sleep' , sleepRouter);
+
+app.listen(config.PORT, () => {
+  console.log('Server listening on port 3000');
+});
